ec87238ddd0bc7cf4e524112f0fa299b5a20566f
Isabele Moreira <79315504+Isabele1997@users.noreply.github.com>
2024-03-25 17:20:59 +0000

    Add files via upload

---
+++ aula_1.js
@@ -0,0 +1,64 @@
+//Atividade em Sala de Aula - 1
+var nome = "Isabele"
+var sobrenome = "Moreira"
+var anoAtual = "2024"
+var idadeateofinaldoano = "27"
+
+//Concaternação
+var resultadoA = nome + " " + sobrenome
+console.log(resultadoA)
+
+var subtracao = anoAtual - idadeateofinaldoano
+console.log("Ano em que nasci", subtracao)
+
+//Atividade em Sala de Aula - 2
+var numero1 = prompt("valor 1")
+var numero2 = prompt("valor 2")
+
+var numero1Convertido = parseFloat(numero1)
+var numero2Convertido = parseFloat(numero2)
+
+var soma = numero1Convertido + numero2Convertido
+console.log(soma)
+
+var produto = numero1Convertido * numero2Convertido
+alert(produto)
+
+//Junto com o Professor
+    var marca = "Adidas"
+    console.log("marca", marca)
+
+    //Valores para Soma
+    var primeiroNumero = 1
+    var segundoNumero = 2
+    var terceiroNumero = 3
+
+    //Soma de dois valores
+    var soma = primeiroNumero + segundoNumero
+    console.log("Soma de dois valores", soma)
+
+    //Subtração de dois valores
+    var subtracao = segundoNumero - primeiroNumero
+    console.log("Subtração de dois valores", subtracao)
+
+    //Multiplicação de dois valores
+    var multiplicacao = primeiroNumero * terceiroNumero
+    console.log("Multiplicação de dois valores", multiplicacao)
+
+    //Divisão de dois valores
+    var divisao = primeiroNumero % terceiroNumero
+    console.log("Divisão de dois valores", divisao)
+
+    //Potencia
+    var potencia = primeiroNumero ** terceiroNumero
+    console.log("Potencia", potencia)
+
+    //Soma de Textos
+
+    var textoA = "Coder"
+    var textoB = "House"
+    var textoC = " "
+
+    //Concaternação
+    var resultadoB = textoA + textoC + textoB
+    console.log(resultadoB)
